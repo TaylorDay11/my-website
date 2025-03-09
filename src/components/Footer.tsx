6b6c9b39208aadad57c088f023f251422b707317
Taylor Hershberger <taylor.day.hershberger@gmail.com>
2025-03-09 14:45:01 -0600

    wider desktop updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,16 +5,18 @@ import { TbBrandGithubFilled } from "react-icons/tb"
 export default function Footer() {
     return (
         <footer>
-            <h3 className="connect">Connect with me:</h3>
-            <a href="https://www.linkedin.com/in/taylor-hershberger-815767164/" target="_blank">
-                <ImLinkedin2 />
-            </a>
-            <a href="https://github.com/TaylorDay11" target="_blank">
-                <TbBrandGithubFilled />
-            </a>
-            <a href="mailto:taylor.day.hershberger@gmail.com" target="_blank">
-                <FaTelegramPlane />
-            </a>
+            <div className="footer-container">
+                <h3 className="connect">Connect with me:</h3>
+                <a href="https://www.linkedin.com/in/taylor-hershberger-815767164/" target="_blank">
+                    <ImLinkedin2 />
+                </a>
+                <a href="https://github.com/TaylorDay11" target="_blank">
+                    <TbBrandGithubFilled />
+                </a>
+                <a href="mailto:taylor.day.hershberger@gmail.com" target="_blank">
+                    <FaTelegramPlane />
+                </a>
+            </div>
         </footer>
     )
 }
